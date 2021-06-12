128982d01e0a4eb001296ebb4c9bf6f92c7ea630
SYED <abul.syed@oup.com>
2021-06-12 13:54:09 +0100

    init

--- post-client/src/router/index.js
+++ post-client/src/router/index.js
@@ -1,11 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+
+const requireAuth = (to, from, next) => {
+  if(!localStorage.getItem('jwt')){
+    next({ name: 'Login' })
+  }else{
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: requireAuth
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   }
 ]
 
---
+++ post-client/src/views/Signup.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="signup">
+    <header>
+      <h3>POST<span>APP</span></h3>
+      <h4>Signup</h4>
+    </header>
+    <form>
+      <input type="text" required placeholder="first name" v-model="firstname">
+      <input type="text" required placeholder="last name" v-model="lastname">
+      <input type="email" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="password" v-model="password">
+      <button>Signup</button>
+      <div v-if="error" class="error">{{ errorMessage }}</div>
+    </form>
+    <footer>
+      <p>Already registered? <router-link :to="{ name: 'Login' }" class="link">Login</router-link></p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      error: false,
+      errorMessage: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+ 
+<style scoped>
+.signup {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+header {
+  margin-top: 1.5rem;
+}
+h3 {
+  color: #000;
+  font-size: 2.8rem;
+  text-align: center;
+  font-weight: 900;
+}
+h4 {
+  color: #888;
+  font-size: 2.4rem;
+  text-align: center;
+  font-weight: 300;
+  margin: 1rem 0;
+}
+span {
+  font-weight: 300;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 2.5rem;
+}
+@media(min-width: 768px) {
+  form {
+    align-self: center;
+    width: 65rem;
+  }
+}
+form input {
+  border: none;
+  outline: none;
+  border: 1px solid #ddd;
+  padding: 1rem;
+  margin-bottom: 1.5rem;
+  background-color: #eee;
+}
+form input:focus {
+  border: 1px solid #aaa;
+}
+form button {
+  border: none;
+  outline: none;
+  padding: 1rem;
+  background: #2580F7;
+  cursor: pointer;
+  border-radius: .5rem;
+  color: #fff;
+  font-size: 1.8rem;
+  font-weight: 700;
+}
+.error {
+  color: rgb(243, 35, 35);
+  text-align: center;
+  padding: 1rem;
+  font-size: 1.4rem;
+}
+footer {
+  width: 100%;
+  height: 6rem;
+  background: rgb(241, 241, 241);
+  padding: 2rem 2.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.2);
+}
+footer p {
+  color: #888;
+  text-align: center;
+  font-size: 1.8rem;
+}
+.link {
+  color: #2580F7;
+  text-decoration: none;
+}
+</style>
